cb85fe561ace356be38d408e23942fd9ebaf9e68
Boris Demeshev <boris.demeshev@gmail.com>
2018-09-27 21:19:06 +0300

    prev gdp deflator

--- scripts/gdp_deflator.R
+++ scripts/gdp_deflator.R
@@ -4,9 +4,12 @@ library(tsibble)
 Sys.setlocale("LC_TIME","C")
 
 data = import("../raw/2018-09-27/tab9a.xls")
+data_previous = import("../raw/2018-09-27/tab9.xls")
 
 data <- t(data[5, ]) %>% na.omit() %>% as.numeric()
-gdp_deflator <- ts(data, start = c(2012, 1), freq = 4)
+data_previous <- t(data_previous[4, ]) %>% na.omit() %>% as.numeric()
+
+gdp_deflator <- ts(c(data_previous, data), start = c(1996, 1), freq = 4)
 gdp_deflator <- as_tsibble(gdp_deflator) %>% rename(date = index)
 
 
