e0370f3d23cca36df8614c52e6beeb64861058f3
Boris Demeshev <boris.demeshev@gmail.com>
2018-12-22 18:17:11 +0300

    no changes 2018-12-22

--- scripts/watchdog.R
+++ scripts/watchdog.R
@@ -2,7 +2,12 @@ library(kassandr)
 
 
 raw_data_folder = "../raw/"
+
+watchdog = rio::import(paste0(raw_data_folder, "watchdog.csv"))
+print(watchdog)
+
 new_watchdog = download_watchdog_files(raw_data_folder)
+print(new_watchdog)
 
 write_csv(new_watchdog, path = "../raw/watchdog.csv")
 
